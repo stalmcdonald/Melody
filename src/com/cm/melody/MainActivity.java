b686a663bb3a2ffb189b1e1cf4590d29e5c07657
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-10-23 23:01:57 -0700

    Music Buttons Created and are responsive
    Added more CSS files

--- src/com/cm/melody/MainActivity.java
+++ src/com/cm/melody/MainActivity.java
@@ -1,8 +1,20 @@
+/*
+-Title: Melody
+-By: Crystal McDonald
+-For: MDF3
+-Assignment: Project 4
+-Term: 1310
+*/
+
 package com.cm.melody;
+
+
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
@@ -11,6 +23,10 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//makes activity full screen
+				requestWindowFeature(Window.FEATURE_NO_TITLE);
+				getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+				                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
 		WebView webView = (WebView)findViewById(R.id.webView);
 		webView.getSettings().setJavaScriptEnabled(true);
