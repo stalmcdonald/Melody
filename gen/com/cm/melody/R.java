d44ed48ca4dc2d39fb31ea5f636d13ef9ae082a9
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-10-24 16:39:41 -0700

    Added Native Feature- custom webview
    custom Webview launches when note is clicked it chimes

--- gen/com/cm/melody/R.java
+++ gen/com/cm/melody/R.java
@@ -11,36 +11,32 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int browsebutton=0x7f020000;
+        public static final int goback=0x7f020001;
+        public static final int goforward=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_launcher1=0x7f020004;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080001;
-        public static final int webView=0x7f080000;
+        public static final int browseButton=0x7f070001;
+        public static final int goBack=0x7f070003;
+        public static final int goForward=0x7f070002;
+        public static final int menu_settings=0x7f070006;
+        public static final int musicalWebView=0x7f070005;
+        public static final int textView1=0x7f070004;
+        public static final int webView=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_web=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
-    }
-    public static final class raw {
-        public static final int piano_a=0x7f040000;
-        public static final int piano_b=0x7f040001;
-        public static final int piano_bb=0x7f040002;
-        public static final int piano_c=0x7f040003;
-        public static final int piano_c2=0x7f040004;
-        public static final int piano_d=0x7f040005;
-        public static final int piano_e=0x7f040006;
-        public static final int piano_eb=0x7f040007;
-        public static final int piano_f=0x7f040008;
-        public static final int piano_f2=0x7f040009;
-        public static final int piano_g=0x7f04000a;
-        public static final int piano_g2=0x7f04000b;
+        public static final int activity_main=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
+        public static final int app_name=0x7f040000;
+        public static final int hello_world=0x7f040001;
+        public static final int menu_settings=0x7f040002;
     }
     public static final class style {
         /** 
@@ -64,10 +60,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f050001;
     }
 }
--- src/com/cm/melody/MainActivity.java
+++ src/com/cm/melody/MainActivity.java
@@ -8,34 +8,90 @@
 
 package com.cm.melody;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends Activity {
+		Button browseButton;//global variables
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//makes activity full screen
-				requestWindowFeature(Window.FEATURE_NO_TITLE);
-				getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-				                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		setContentView(R.layout.activity_main);
+		
 		//finding my custom view
 		WebView webView = (WebView)findViewById(R.id.webView);
 		//enables view to be posted to webview
 		webView.getSettings().setJavaScriptEnabled(true);
-		webView.setWebChromeClient(new WebChromeClient());
+		webView.setWebChromeClient(new WebChromeClient());//responsible for everything browser UI specific
+		
 		//referencing the www files (Html, CSS, and JS)
-		webView.loadUrl("file:///android_asset/www/index.html");
+		webView.loadUrl("file:///android_asset/www/index.html");//responsible for everything that is related to the rendering of the web content
+		
+		browseButton = (Button) findViewById(R.id.browseButton);
+		
+		browseButton.setOnClickListener(new OnClickListener() {//setup onclick listener
+		
+		/*
+		 * Implicit Intent: Launches Web Browsers to open website
+		 */
+			public void onClick(View webview) {
+				// sends user to site that shows more information on sailing in San Francisco
+	Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://www.teoria.com/tutorials/reading/12-notes.php"));
+	startActivity(viewIntent); 
+	playsound();
+};	
+		});
 	}
+	
+	
+	
+	
+	@JavascriptInterface
+	private void playsound() {
+
+		        try {
+		        	//set up MediaPlayer    
+			        MediaPlayer mp = new MediaPlayer();
+		        	//grabbing soundfile from assets
+		            AssetFileDescriptor descriptor = getAssets().openFd("piano_c.mp3");
+		          
+		            mp.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+		            descriptor.close();
+		            mp.prepare();
+		            mp.start();
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		            Log.e("AssetFileDescriptor", e.toString());
+		            playsound();
+		        }
+		}
+	
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
